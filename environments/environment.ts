0e2ea880438d450d04452ac1c735552c50d24d46
waqar-nawaz <waqar@replatechnologies.com>
2024-09-08 21:56:49 +0500

    set enviroments anme

---
+++ environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://ecommerce-website-backend-1-52ky.onrender.com/',
+  fileUrl: 'https://ecommerce-website-backend-1-52ky.onrender.com/',
+};
