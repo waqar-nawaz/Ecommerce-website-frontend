0203f3c51e16957f9cdc5c38aa74d039d9a5ad54
waqar-nawaz <waqar@replatechnologies.com>
2024-09-08 22:20:58 +0500

    fix image Delete popup to show

--- src/app/pages/productlist/about.component.ts
+++ src/app/pages/productlist/about.component.ts
@@ -29,6 +29,7 @@ export class AboutComponent implements OnInit {
   products = inject(ProductServiceService);
   refreshService = inject(RefreshService);
   authService = inject(AuthService);
+  sharedService = inject(SharedService);
   loader: boolean = false;
   search: FormControl = new FormControl();
 
@@ -84,9 +85,9 @@ export class AboutComponent implements OnInit {
       if (result.isConfirmed) {
         this.products.deleteProduct(data._id).subscribe(
           (res: any) => {
-            console.log('delet images', res);
+            console.log('delet images', res?.message);
             // this.maketoster();
-            inject(SharedService).maketoster({
+            this.sharedService.maketoster({
               success: 'success',
               message: res?.message,
             });
