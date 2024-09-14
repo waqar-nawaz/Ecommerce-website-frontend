c5f3a36bb38abe1b094dde911ad9ed2173cdc37f
waqar-nawaz <waqar@replatechnologies.com>
2024-09-14 17:10:18 +0500

    changes

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,13 +1,13 @@
-// export const environment = {
-//   production: true,
-//   apiUrl: 'https://ecommerce-website-backend-1-52ky.onrender.com/',
-//   fileUrl: 'https://ecommerce-website-backend-1-52ky.onrender.com/',
-// };
+export const environment = {
+  production: true,
+  apiUrl: 'https://ecommerce-website-backend-1-52ky.onrender.com/',
+  fileUrl: 'https://ecommerce-website-backend-1-52ky.onrender.com/',
+};
 
 //for Locall
 // src/environments/environment.ts
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/',
-  fileUrl: 'http://localhost:8080/',
-};
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:8080/',
+//   fileUrl: 'http://localhost:8080/',
+// };
