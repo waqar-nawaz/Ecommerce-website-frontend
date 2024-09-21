32f421bee122f6669a5f02acd8f6c0e786c9332b
waqar-nawaz <waqar@replatechnologies.com>
2024-09-22 01:39:01 +0500

    add contact form

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,23 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ModalComponent } from '../../components/modal/modal.component';
-import Swal from 'sweetalert2';
 import jsonarray from '../../../../dummy.json';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ModalComponent, FormsModule, CommonModule],
+  imports: [ModalComponent, FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
-export class ContactComponent {
-  onFileSelected($event: Event) {
-    throw new Error('Method not implemented.');
-  }
+export class ContactComponent implements OnInit {
   data: any[] = jsonarray;
   list: string = '';
+  contactForm!: FormGroup;
+  successMessage: string | null = null;
+  fb = inject(FormBuilder);
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.contactFrom();
+  }
+
+  // Initialize the form group
+  contactFrom() {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      // Perform form submission logic here (e.g., send the contact details to a backend server)
+      this.successMessage = 'Thank you! Your message has been sent.';
+
+      // Reset the form after submission
+      this.contactForm.reset();
+    } else {
+      // Handle form errors
+      this.successMessage = null;
+    }
+  }
 }
