7aa80485dbafb8de3c4a9d00aac3cc967f21f5c5
waqar-nawaz <waqar@replatechnologies.com>
2024-09-29 16:47:20 +0500

    change env

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,17 +1,17 @@
-// export const environment = {
-//   production: true,
-//   apiUrl: 'https://ecommerce-website-backend-1-52ky.onrender.com/',
-//   fileUrl: 'https://ecommerce-website-backend-1-52ky.onrender.com/',
-//   defaultImage: 'https://loading.io/assets/mod/spinner/spinner/lg.gif',
-//   domainPath: 'https://ecommerce-website-backend-1-52ky.onrender.com/',
-// };
+export const environment = {
+  production: true,
+  apiUrl: 'https://ecommerce-website-backend-1-52ky.onrender.com/',
+  fileUrl: 'https://ecommerce-website-backend-1-52ky.onrender.com/',
+  defaultImage: 'https://loading.io/assets/mod/spinner/spinner/lg.gif',
+  domainPath: 'https://ecommerce-website-backend-1-52ky.onrender.com/',
+};
 
 //for Locall
 // src/environments/environment.ts
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/',
-  fileUrl: 'http://localhost:8080/',
-  defaultImage: 'https://loading.io/assets/mod/spinner/spinner/lg.gif',
-  domainPath: 'http://localhost:8080/',
-};
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:8080/',
+//   fileUrl: 'http://localhost:8080/',
+//   defaultImage: 'https://loading.io/assets/mod/spinner/spinner/lg.gif',
+//   domainPath: 'http://localhost:8080/',
+// };
