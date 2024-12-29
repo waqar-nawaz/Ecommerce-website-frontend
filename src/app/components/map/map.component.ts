96515229070d7ec4efb8cbca09943287f3d88604
waqar-nawaz <waqar@replatechnologies.com>
2024-12-30 00:27:39 +0500

    changes

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -7,62 +6,34 @@ import * as L from 'leaflet';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-  private map!: L.Map;
-
-  locations = [
-    { lat: 30.3753, lng: 69.3451, title: "Pakistan" },
-    { lat: 20.5937, lng: 78.9629, title: "India" },
-    { lat: -25.2744, lng: 133.7751, title: "Australia" },
-    { lat: 35.8617, lng: 104.1954, title: "China" },
-    { lat: 51.1657, lng: 10.4515, title: "Germany" }
-  ];
+  private map: any;
 
   ngOnInit(): void {
     if (typeof window !== 'undefined') {
-      this.initializeLeaflet();
+      this.loadLeaflet();
     }
   }
 
-  private initializeLeaflet(): void {
+  private loadLeaflet(): void {
     import('leaflet').then(L => {
-      this.initializeMap(L);
+      this.initMap(L);
     });
   }
 
-  private initializeMap(L: any): void {
-    this.map = L.map('map', {
-      center: [20.5937, 78.9629], // Centered on India
-      zoom: 2,
-      zoomControl: false, // Disable default zoom control
-      attributionControl: false // Disable default attribution control
-    });
+  private initMap(L: any): void {
+    this.map = L.map('map').setView([51.505, -0.09], 13);
 
-    L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-      attribution: 'Map data &copy; <a href="https://www.google.com/intl/en_ALL/mapfiles/terms.html">Google</a>',
-      subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap'
     }).addTo(this.map);
 
-    // Custom zoom control
-    L.control.zoom({
-      position: 'bottomright'
-    }).addTo(this.map);
-
-    // Custom attribution
-    L.control.attribution({
-      position: 'bottomleft'
-    }).addAttribution('Map data &copy; <a href="https://www.google.com/intl/en_ALL/mapfiles/terms.html">Google</a>');
-
-    this.addMarkers(L);
+    this.addMarker(L);
   }
 
-  private addMarkers(L: any): void {
-    this.locations.forEach(location => {
-      L.marker([location.lat, location.lng], {
-        icon: L.divIcon({
-          className: 'custom-icon',
-          html: `<div class="marker-pin"></div><div class="marker-label">${location.title}</div>`,
-        })
-      }).addTo(this.map);
-    });
+  private addMarker(L: any): void {
+    const marker = L.marker([51.5, -0.09]).addTo(this.map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
   }
 }
