42bdddca6716a62a1aa9214ef966e20092667875
waqar-nawaz <waqar@replatechnologies.com>
2024-12-30 00:24:49 +0500

    change

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -9,7 +9,6 @@ import * as L from 'leaflet';
 export class MapComponent implements OnInit {
   private map!: L.Map;
 
-  // Define multiple locations with latitude, longitude, and title
   locations = [
     { lat: 30.3753, lng: 69.3451, title: "Pakistan" },
     { lat: 20.5937, lng: 78.9629, title: "India" },
@@ -24,31 +23,46 @@ export class MapComponent implements OnInit {
     }
   }
 
-  // Dynamically load Leaflet library
   private initializeLeaflet(): void {
     import('leaflet').then(L => {
       this.initializeMap(L);
     });
   }
 
-  // Initialize the Leaflet map with Google Maps layer
   private initializeMap(L: any): void {
-    this.map = L.map('map').setView([20.5937, 78.9629], 2); // Centered on India
+    this.map = L.map('map', {
+      center: [20.5937, 78.9629], // Centered on India
+      zoom: 2,
+      zoomControl: false, // Disable default zoom control
+      attributionControl: false // Disable default attribution control
+    });
 
     L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
       attribution: 'Map data &copy; <a href="https://www.google.com/intl/en_ALL/mapfiles/terms.html">Google</a>',
       subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
     }).addTo(this.map);
 
+    // Custom zoom control
+    L.control.zoom({
+      position: 'bottomright'
+    }).addTo(this.map);
+
+    // Custom attribution
+    L.control.attribution({
+      position: 'bottomleft'
+    }).addAttribution('Map data &copy; <a href="https://www.google.com/intl/en_ALL/mapfiles/terms.html">Google</a>');
+
     this.addMarkers(L);
   }
 
-  // Load all markers onto the map
   private addMarkers(L: any): void {
     this.locations.forEach(location => {
-      L.marker([location.lat, location.lng]).addTo(this.map)
-        .bindPopup(location.title)
-        .openPopup();
+      L.marker([location.lat, location.lng], {
+        icon: L.divIcon({
+          className: 'custom-icon',
+          html: `<div class="marker-pin"></div><div class="marker-label">${location.title}</div>`,
+        })
+      }).addTo(this.map);
     });
   }
 }
