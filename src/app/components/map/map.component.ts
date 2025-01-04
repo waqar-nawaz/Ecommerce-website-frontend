26076ba052e8cc2e843a30f44da6bc365a171684
waqar-nawaz <waqar@replatechnologies.com>
2025-01-05 00:22:51 +0500

    change

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -52,6 +52,13 @@ export class MapComponent implements OnInit {
 
 
     this.addMarkers(L);
+
+    delete (L.Icon.Default.prototype as any)._getIconUrl;
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: 'assets/leaflet/marker-icon-2x.png',
+      iconUrl: 'assets/leaflet/marker-icon.png',
+      shadowUrl: 'assets/leaflet/marker-shadow.png',
+    });
   }
 
   private addMarkers(L: any): void {
