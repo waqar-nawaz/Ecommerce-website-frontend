e41f0d99bb054dc2b2ba21bb40ad05c2a42deae9
waqar-nawaz <waqar@replatechnologies.com>
2025-02-13 00:21:19 +0500

    test production

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ecommerce-website-backend-1-52ky.onrender.com/',
+  apiUrl: 'https://ecommerce-website-backend-1-52ky.onrender.com/api/v1',
   fileUrl: 'https://ecommerce-website-backend-1-52ky.onrender.com/',
+  defaultImage: 'https://loading.io/assets/mod/spinner/spinner/lg.gif',
+  domainPath: 'https://ecommerce-website-backend-1-52ky.onrender.com/',
+  googleClientId: '365188175820-l346s67vb26c96n97kaaaqlr1ekarpa1.apps.googleusercontent.com',
+  googleClientSecret: 'Q6J9J9GOCSPX-cU6DbnZXQSoJeX2D2YyFQOohUtFf'
 };
